73ffa6bf39abeced7f0035d8a8f144682731a43b
koppai <psthu.koppai@gmail.com>
2024-02-05 22:26:01 +0630

    Add detail screen

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,17 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { DetailsScreenProp, ITodo } from '../types';
 
-const DetailsScreen = ({ route }: { route: DetailsScreenProp }) => {
+// Import types
+import { DetailsRouteProp, ITodo } from '../types';
+
+const DetailsScreen = ({ route }: { route: DetailsRouteProp }) => {
   const item: ITodo = route.params;
 
   return (
-    <View>
-      <Text>{item.title}</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Completed: {item.completed ? 'True' : 'False'}</Text>
+      <Text style={styles.text}>Titile: {item.title.toUpperCase()}</Text>
+      <Text style={styles.text}>Description: {item.description}</Text>
     </View>
   );
 };
 
 export default DetailsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: 'white',
+    rowGap: 10,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
